4c97a5641d17a444c0948a540ae3708372162de2
Sivanandhana <sivanandhana2308@gmail.com>
2026-01-19 22:16:02 +0530

    add get post methods

--- Employee/urls.py
+++ Employee/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emp_app.views import EmployeeCreateListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("detials/",EmployeeCreateListView.as_view()),
 ]
--- emp_app/views.py
+++ emp_app/views.py
@@ -6,8 +6,37 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from json import loads
 
+@method_decorator(csrf_exempt,name="dispatch")
 class EmployeeCreateListView(View):
 
     def get(self,request,*args,**kwargs):
+         
+         qs = Employee.objects.all()
 
-        
+         result =[{id:emp.id,"name":emp.name,"department":emp.department, "salary":emp.salary,"location":emp.location,"age":emp.age,"experience":emp.experience}for emp in qs]
+
+         return JsonResponse({"data":result,"status":"200 ok"})
+    
+    def post(self,request,*args,**kwargs):
+         
+         data = loads(request.body)
+
+         qs = Employee.objects.create(
+              
+              name = data.get("name"),
+
+              department = data.get("department"),
+
+              salary = data.get("salary"),
+
+              location = data.get("location"),
+
+              age = data.get("age"),
+
+              experience = data.get("experience")
+
+         )
+
+         return JsonResponse({"data":"record inserted","status":"200 success"})
+    
+
